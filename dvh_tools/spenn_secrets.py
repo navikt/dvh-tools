29dc6e8e7a67bceadf7e272f81d1d76f24bf13bd
caroliwi <caroline.winther@nav.no>
2024-04-30 08:01:12 +0000

    lagt til generell funksjon for hemmelig

--- dvh_tools/spenn_secrets.py
+++ dvh_tools/spenn_secrets.py
@@ -2,7 +2,16 @@ import json
 
 from google.cloud import secretmanager
 
+def get_gsm_secret(project_id, secret_name):
+    '''Returnerer secret-verdien
+    '''
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    secret = json.loads(response.payload.data.decode('UTF-8'))
+    return secret
 
+## skal fases ut:
 class SpennSecrets:
     def __init__(self, secret_id, environment):
         '''
