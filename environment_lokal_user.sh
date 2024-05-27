14b09cbc588f3d0e4ec2a4f7da93735b4eb48ab1
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2024-05-27 18:25:07 +0200

    .sh liker ikke å og ø, fjerner det

--- environment_lokal_user.sh
+++ environment_lokal_user.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# setter miljøvariabler for å kjøre lokalt i knada-vm
-# husk å kjøre chmod +x environment_lokal_user.sh for å gjøre filen kjørbar
-# og så må fila kjøres hver gang i ny terminal med source environment_lokal_user.sh
+# setter miljovariabler for a kjore lokalt i knada-vm
+# husk a kjore chmod +x environment_lokal_user.sh for a gjore filen kjorbar
+# og sa ma fila kjores hver gang i ny terminal med source environment_lokal_user.sh
 
 read -p "Enter username (evt med proxy-klammer): " LOKAL_USER
 export LOKAL_USER
 
-# passordet er skjult når det skrives inn. Det blir bare lagret i minnet, ikke i en fil
+# passordet er skjult nar det skrives inn. Det blir bare lagret i minnet, ikke i en fil
 read -sp "Enter password: " LOKAL_PASSWORD
 export LOKAL_PASSWORD
