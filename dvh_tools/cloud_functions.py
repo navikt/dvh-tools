1b917de070c0b60278bf11a548307bee8f20be33
caroliwi <caroline.winther@nav.no>
2024-07-30 09:27:05 +0000

    lagt til nye funksjoner for å interagere med google buckets

--- dvh_tools/cloud_functions.py
+++ dvh_tools/cloud_functions.py
@@ -1,6 +1,8 @@
 import json
+from io import BytesIO
 from google.cloud import secretmanager
 from google.oauth2 import service_account
+from google.cloud import storage
 from google.cloud import bigquery
 import pandas as pd
 from google.cloud.exceptions import NotFound
@@ -10,7 +12,20 @@ import logging
 
 
 def get_gsm_secret(project_id, secret_name):
-    """Returnerer secret-verdien"""
+    """Gets the value of the secret into a python object
+
+    Parameters
+    ----------
+    project_id : str
+        Name of Google project
+    secret_name : str
+        name of secret in Google Secret Manager
+
+    Returns
+    -------
+    object
+        The value in the json in the secret
+    """
     client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
     response = client.access_secret_version(request={"name": name})
@@ -80,3 +95,54 @@ def trunc_and_load_to_bq(
         table.description = bq_table_description
         bq_client.update_table(table, ["description"])
         logging.info(f"Updated table description for {bq_target}")
+
+
+def create_storage_client(project_id: str, secret_name_bucket: str):
+    """Creates a Google storage client used for interacting with buckets
+
+    Parameters
+    ----------
+    project_id : str
+        Name of Google project
+    secret_name_bucket : str
+        Name of GSM secret where the key to the service user is stored
+
+    Returns
+    -------
+    google.cloud.storage.client.Client
+        storage client that can be used to interacting with buckets
+    """
+    bucket_secret = get_gsm_secret(project_id, secret_name_bucket)
+    creds = service_account.Credentials.from_service_account_info(bucket_secret)
+    storage_client = storage.Client(credentials=creds, project=creds.project_id)
+    return storage_client
+
+
+def from_gcs_to_df(bucket_name, client, filename, sep):
+    """Reads a csv-file from a bucket into a pandas dataframe
+
+    Parameters
+    ----------
+    bucket_name : str
+        name of bucket
+    client : google.cloud.storage.client.Client
+        storage client
+    filename :
+        name of csv-file
+    sep : str
+        separator used in the csv
+
+    Returns
+    -------
+    pandas.dataframe
+        A pandas dataframe of the data in the csv-file
+    """
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+    byte_stream = BytesIO()
+    blob.download_to_file(byte_stream)
+    byte_stream.seek(0)
+    if filename.lower().endswith('.csv'):
+        df = pd.read_csv(byte_stream, sep=sep)
+    del blob, byte_stream
+    return df
