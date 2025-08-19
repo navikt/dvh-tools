67b4b043fc6b60bdf081ff2b55130fc9dd48bf44
delmona <delmon.arous@nav.no>
2025-08-19 10:55:09 +0200

    Fjerner `get_comments_from_oracle` og `generate_comments_from_sql` fra `__init__`

--- dvh_tools/dbt_tools/__init__.py
+++ dvh_tools/dbt_tools/__init__.py
@@ -1,6 +1,4 @@
 from .dbt_docs_publish import publish_docs
-from .get_comments_from_oracle import get_comments_from_oracle
-from .generate_comments_from_sql import generate_comments_from_sql
 from .dbt_run_airflow import dbt_run_airflow
 
-__all__ = ["publish_docs", "get_comments_from_oracle", "generate_comments_from_sql", "dbt_run_airflow"]
+__all__ = ["publish_docs", "dbt_run_airflow"]
